5083f6ed9a9b41801e05f23d130819f563fe7760
chipa014 <alextheracoonthesecond@gmail.com>
2023-06-20 18:03:13 +0700

    modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Button.js 	new file:   src/Button.module.css 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import styles from "./Button.module.css";
+
+const Button = function (props) {
+  const close = function () {
+    window.open("", "_self").close();
+  };
+
+  return (
+    <button class={styles.btn} onClick={close}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
